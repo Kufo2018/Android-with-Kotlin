d9fa1366039f558630c67a45ed9fa3ac644c3729
Kufo2018 <ibukunolwalewa@gmail.com>
2019-05-31 23:48:53 +0100

    Add navigation.XML, implement navigation graph

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -8,6 +8,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameBinding
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
@@ -30,6 +32,27 @@ class TitleFragment : Fragment() {
                 inflater, R.layout.fragment_title, container, false
         )
 
+        // Sets onClick listener for play button
+        binding.playButton.setOnClickListener { view: View ->
+            /**
+             * At this point we need to navigate between our destinations
+             * To do this, we need to find an instance of the Navigation Controller
+             * The Navigation Controller is a class that we use to manage navigation within our navigation host fragment
+             * The Navigation host is the parent in the view hierarchy of our current fragment
+             * Navigation provides a helper function called findNavController
+             * findNavController takes a view, finds the enclosing nav host fragment and returns the navigation controller for that nav host fragment
+             */
+            Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+
+            //OR
+            /**
+            *binding.playButton.setOnClickListener {
+            *    Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+            *}
+             */
+
+        }
+
         // Returns the root of the layout we just inflated
         return binding.root
 
@@ -38,7 +61,6 @@ class TitleFragment : Fragment() {
          * But, it is not yet contained by our activity, so it's not visible
          * Go to activity main layout and add the fragment there
          */
-
     }
 
 
